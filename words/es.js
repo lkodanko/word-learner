1ca23b0cb35b3eaba02e99d626cbf5abb115576d
lkodanko <46070812+lkodanko@users.noreply.github.com>
2025-09-09 08:26:30 -0600

    Create es.js

---
+++ words/es.js
@@ -0,0 +1,6 @@
+const WORDS_ES = {
+  3: ["sol", "luz", "mar", "pan", "sal", "ojo", "rey", "voz", "fin", "paz"],
+  4: ["casa", "luna", "gato", "perro", "agua", "flor", "mano", "niño", "jugar", "leer"],
+  5: ["feliz", "amigo", "verde", "libro", "comer", "beber", "fruta", "noche", "queso", "cinco"],
+  6: ["escuela", "familia", "fiesta", "guitarra", "hermano", "mañana", "naranja", "plátano", "blanco", "viernes"]
+};
