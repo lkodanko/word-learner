cc2f7116ad55045a2d7404ef50682e7a770f04e3
lkodanko <46070812+lkodanko@users.noreply.github.com>
2025-09-09 08:26:48 -0600

    Create fr.js

---
+++ words/fr.js
@@ -0,0 +1,6 @@
+const WORDS_FR = {
+  3: ["vie", "ami", "eau", "feu", "jeu", "roi", "sel", "ciel", "mer", "nuit"],
+  4: ["joue", "rose", "main", "pied", "livre", "bleu", "chat", "chien", "lune", "pain"],
+  5: ["table", "chaise", "pomme", "maison", "école", "lettre", "merci", "fleur", "monde", "plage"],
+  6: ["soleil", "enfant", "fenêtre", "fromage", "banane", "orange", "samedi", "manger", "dormir", "parler"]
+};
