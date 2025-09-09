2db764b5ce190ec19a8dedfea564334003b6e722
lkodanko <46070812+lkodanko@users.noreply.github.com>
2025-09-09 08:26:08 -0600

    Create en.js

---
+++ words/en.js
@@ -0,0 +1,6 @@
+const WORDS_EN = {
+  3: ["cat", "dog", "sun", "run", "big", "red", "egg", "fun", "fly", "try"],
+  4: ["ball", "tree", "fish", "frog", "jump", "play", "star", "moon", "sing", "read"],
+  5: ["apple", "happy", "smile", "learn", "water", "earth", "house", "music", "story", "magic"],
+  6: ["banana", "friend", "school", "purple", "window", "summer", "animal", "flower", "family", "listen"]
+};
